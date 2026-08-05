ad8168ee4965513975462f89fd669da24afe313f
PorygonVault <contact@porygonvault.com>
2026-08-05 05:31:09 +0200

    Create 006.ts

---
+++ data-asia/ADV/ADV1/006.ts
@@ -0,0 +1,71 @@
+import { Card } from "../../../interfaces";
+import Set from "../ADV1";
+
+const card: Card = {
+    set: Set,
+    name: {
+        ja: "アゲハント",
+    },
+
+    illustrator: "Hajime Kusajima",
+    rarity: "Rare",
+    category: "Pokemon",
+    dexId: [267],
+    hp: 80,
+    types: ["Grass"],
+    stage: "Stage1",
+  
+    evolveFrom: {
+        ja: "カラサリス",
+    },
+
+    abilities: [
+      {
+        type: "ポケボデー",
+        name: {
+          ja: "だつりょくりんぶぶん",
+        },
+        effect: {
+          ja: "このポケモンがいるかぎり、おたがいのバトルポケモン全員の抵抗力は、すべてなくなる。",
+        },
+      },
+    ],
+  
+    attacks: [
+      {
+            cost: ["Grass"],
+            name: {
+                ja: "しびれごな",
+            },
+            effect: {
+                ja: "コインを1回投げオモテなら、相手をマヒにする。",
+            damage: 20,
+      },
+      {
+            cost: ["Grass", "Colorless", "Colorless"],
+            name: {
+                ja: "パラレルゲイン",
+            },
+            effect: {
+                ja: "自分のポケモン全員のダメージカウンターを、それぞれ1個ずっとりのぞく。",
+            },
+            damage: 50,
+      },
+  ],
+
+  variants: [
+    {
+          type: "normal",
+          thirdparty: {
+              cardmarket: 784490,
+              tcgplayer: 613054,
+          },
+      },
+  ],
+  
+  retreat: 0,
+  weaknesses: [{ type: "Fire", value: "x2" }],
+  resistances: [],
+};
+
+export default card;
