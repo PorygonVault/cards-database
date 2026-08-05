59f1a69a0a1b9e4b8cff8068cf9abf3eff3540af
PorygonVault <contact@porygonvault.com>
2026-08-05 05:02:07 +0200

    Create 001.ts

---
+++ data-asia/ADV/ADV1/001.ts
@@ -0,0 +1,53 @@
+import { Card } from "../../../interfaces";
+import Set from "../ADV1";
+
+const card: Card = {
+    set: Set,
+    name: {
+        ja: "ドガース",
+    },
+
+    illustrator: "Atsuko Nishida",
+    rarity: "Common",
+    category: "Pokemon",
+    dexId: [109],
+    hp: 50,
+    types: ["Grass"],
+    stage: "Basic",
+
+    attacks: [
+      {
+            cost: ["Colorless"],
+            name: {
+                ja: "たいあたり",
+            },
+            damage: 10,
+      },
+      {
+            cost: ["Grass", "Colorless"],
+            name: {
+                ja: "すてみタックル",
+            },
+            effect: {
+                ja: "自分にも10ダメージ。",
+            },
+            damage: 30,
+      },
+  ],
+
+  variants: [
+    {
+          type: "normal",
+          thirdparty: {
+              cardmarket: 784485,
+              tcgplayer: 613042,
+          },
+      },
+  ],
+  
+  retreat: 1,
+  weaknesses: [{ type: "Psychic", value: "x2" }],
+  resistances: [],
+};
+
+export default card;
