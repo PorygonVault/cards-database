b0e943de9f443048632cb122d2fb15b6dfdc04a1
PorygonVault <contact@porygonvault.com>
2026-08-05 20:18:55 +0200

    Update 004.ts

--- data-asia/ADV/ADV1/004.ts
+++ data-asia/ADV/ADV1/004.ts
@@ -39,7 +39,7 @@ const card: Card = {
   variants: [
     {
           type: "normal",
-          thirdparty: {
+          thirdParty: {
               cardmarket: 784488,
               tcgplayer: 613051,
           },
