6aae0ca1b0b4523ff811a38a50ab8e2bb0d3b332
PorygonVault <contact@porygonvault.com>
2026-08-05 05:47:16 +0200

    Add files via upload

---
+++ data-asia/ADV/ADV1/031.ts
@@ -0,0 +1,69 @@
+import { Card } from "../../../interfaces";
+import Set from "../ADV1";
+
+const card: Card = {
+    set: Set,
+    name: {
+        ja: "",
+    },
+
+    illustrator: "",
+    rarity: "",
+    category: "",
+    dexId: [],
+    hp: ,
+    types: [""],
+    stage: "",
+  
+    evolveFrom: {
+        ja: "",
+    },
+
+    abilities: [
+      {
+        type: "",
+        name: {
+          ja: "",
+        },
+        effect: {
+          ja: "",
+        },
+      },
+    ],
+  
+    attacks: [
+      {
+            cost: [""],
+            name: {
+                ja: "",
+            },
+            damage: ,
+      },
+      {
+            cost: [""],
+            name: {
+                ja: "",
+            },
+            effect: {
+                ja: "",
+            },
+            damage: ,
+      },
+  ],
+
+  variants: [
+    {
+          type: "",
+          thirdparty: {
+              cardmarket: ,
+              tcgplayer: ,
+          },
+      },
+  ],
+  
+  retreat: ,
+  weaknesses: [{}],
+  resistances: [],
+};
+
+export default card;
---
+++ data-asia/ADV/ADV1/053.ts
@@ -0,0 +1,31 @@
+import { Card } from "../../../interfaces";
+import Set from "../M1L";
+
+const card: Card = {
+	set: Set,
+	name: {
+		ja: "",
+	},
+
+	illustrator: "",
+	category: "Trainer",
+  trainerType: "",
+	rarity: "",
+
+	effect: {
+		ja: "",
+	},
+
+ variants: [
+    {
+          type: "",
+          thirdparty: {
+              cardmarket: ,
+              tcgplayer: ,
+          },
+      },
+  ],
+	
+};
+
+export default card;
